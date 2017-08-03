a024e966195f4350cc6b436818ab96578aaf940d
William Huang <williamhuang@Williams-MacBook-Pro.local>
2017-08-02 21:34:07 -0700

    radio buttons

--- src/components/multipleChoice.js
+++ src/components/multipleChoice.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Header from './Header.js';
+import RadioForm, {RadioButton, RadioButtonInput, RadioButtonLabel} from 'react-native-simple-radio-button';
+
+
+
+
 
 export default class multipleChoice extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      types1: [{label: '10 ', value: 0}, {label: '20', value: 1}, {label:30, value:2}, {label:40, value:3}, {label:50, value:4}],
+      value1: 0,
+      value1Index: 0,
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header title = "Question"/>
-        <Text style = {styles.textStyle}>MC Question Page text text text</Text>
+        <ScrollView>
+
+            <Header title = "Question"/>
+            <Text style = {styles.question}>1. A 100 N Force acts on a 5 kg ball. What is its acceleration in meters per second squared?? </Text>
+            <View style={styles.component}>
+              <RadioForm
+                ref="radioForm"
+                radio_props={this.state.types1}
+                initial={0}
+                formHorizontal={false}
+                labelHorizontal={true}
+                buttonColor={'#2196f3'}
+                labelColor={'#000'}
+                animation={true}
+                onPress={(value, index) => {
+                  this.setState({
+                    value1:value,
+                    value1Index:index
+                  })
+                }}
+            />
+            </View>
+          <View style={styles.component}>
+            <Text style={styles.question}> Canvas goes here (but bigger) </Text>
+          </View>
+
+        </ScrollView>
       </View>
     );
   }
@@ -16,12 +56,23 @@ export default class multipleChoice extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
+    backgroundColor: '#F5FCFF'
   },
 
-  textStyle: {
-    fontSize: 20
+  question: {
+    fontSize: 20,
+    textAlign: 'left',
+    marginTop: 20,
+    marginBottom: 20,
+    marginLeft: 5
+  },
+
+  component: {
+    alignItems: 'flex-start',
+    marginBottom: 50,
+    marginLeft: 15
   }
+
+
 });
